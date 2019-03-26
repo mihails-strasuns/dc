737192dd18ab50a95823c7dd609de5b45d58d997
Mihails Strasuns <mihails.strasuns@gmail.com>
2019-03-26 10:59:25 +0000

    Split Windows and Linux build script
    There are too many platform specific bits to put any effort into
    maintaining single build definition right now.

--- ci/linux.sh
+++
@@ -1,2 +0,0 @@
-$bindir/dub test -a x86_64
-$bindir/dub build -a x86_64
--- ci/windows.bat
+++
@@ -1,3 +0,0 @@
-call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" x64
-%bindir%\dub test -a x86_64
-%bindir%\dub build -a x86_64
